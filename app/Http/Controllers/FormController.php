6c10207e8dde3bf1736e13820a79742b70679b20
Josh Lim <43092357+josh-lim1234@users.noreply.github.com>
2018-11-08 09:28:57 +0800

    Update and rename BookController.php to FormController.php

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BookController extends Controller
+class FormController extends Controller
 {
     public function index()
     {
